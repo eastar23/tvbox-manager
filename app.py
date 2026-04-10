b823b263800e42ef0d9b2b9808c21ede2c2f82bb
雷芒 <dongdongdongdongqiang@Mac-mini.local>
2026-04-10 17:05:38 +0800

    feat: enable universal multi-platform docker support (v1.0.26)

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 
 # --- Configuration & Constants ---
-APP_VERSION = "v1.0.25"
+APP_VERSION = "v1.0.26"
 app.secret_key = os.environ.get('SECRET_KEY', 'super-secret-starlink-clone-key')
 DATABASE = os.environ.get('DB_PATH', '/app/data/database.db')
 REG_CODE = os.environ.get('REG_CODE', '888888')
